b71e91fd3dd67888c70b8c8c77cc74496c6280cd
Naz-Amer <smith2011@inbox.ru>
2023-12-11 13:45:06 -0600

    seed data

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 ﻿namespace CodeFirstExistingDatabase.Migrations
 {
     using System;
+    using System.Collections.ObjectModel;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +19,17 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author
+                {
+                    Name = "Author 1",
+                    Courses = new Collection<Course>
+                    {
+                        new Course {Name = "Course for Author 1",Description ="Description"}
+                    }
+                });
+
         }
     }
 }
